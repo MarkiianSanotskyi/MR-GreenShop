f534144252add7144e8e37c37ba034dff5dd8962
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-14 10:09:57 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+/*placeholder*/
+$(document).ready(function(){
+        $.Placeholder.init({ color : "#a3a3a3" });
+ });
+
+(function($) {
+		$(function() {
+			$('input, select').styler({
+				selectSearch: true
+			});
+		});
+		})(jQuery);
+		
+			/*Fancybox*/
+$(document).ready(function(){
+
+            var forms = $('.form-modal'),
+            cb_input = forms.find('input[type=text], input[type=email], input[type=tel] , textarea'),
+            cr_close = $.fancybox.close();
+
+    $("#fancybox-overlay").fancybox({
+        padding: [0, 0, 0, 0],
+        margin: [120, 0, 0, 0],
+        scrolling: 'visible',
+        minWidth: 307,
+        minHeight: 141,
+        autoSize: false,
+        autoHeight: true,
+        autoWidth: true,
+        maxWidth: 9999,
+        maxHeight: 9999,
+        fixed: false,
+        autoCenter: false,
+        closeBtn: true,
+        afterClose: function () {
+            cb_input.val('');
+        }
+    });
+
+    $('.btr').click(function () {
+        $.fancybox.close();
+    });
+	
+   });
+/*Fancybox end*/ 
